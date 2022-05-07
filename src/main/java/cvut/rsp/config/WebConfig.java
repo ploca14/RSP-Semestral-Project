fed1077cf905883bd25291fd5b397a535a0b4c4f
rudenvad <104729209+rudenvad@users.noreply.github.com>
2022-05-07 11:09:54 +0200

    Access to graphiql page fixed by extending configuration

--- src/main/java/cvut/rsp/config/WebConfig.java
+++ src/main/java/cvut/rsp/config/WebConfig.java
@@ -4,18 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
-
+public class WebConfig implements WebMvcConfigurer
+{
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry)
+    {
         registry.addMapping("/graphql/**")
             .allowedOrigins("http://localhost:3000")
             .allowedHeaders(CorsConfiguration.ALL)
             .allowedMethods(CorsConfiguration.ALL);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        registry.addResourceHandler("/vendor/**")
+                .addResourceLocations("classpath:/static/vendor/");
     }
 }
